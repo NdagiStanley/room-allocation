4c404bf3e15810f10535f027d54d1c2f8b92ef32
andela-sndagi <stanley.ndagi@andela.com>
2016-01-05 18:03:51 +0300

    (task) Task 1 complete:Pre-populate model with 10 of each type of room

--- model.py
+++ model.py
@@ -1,21 +1,20 @@
 """
-    Model for the rooms at Amity
+    Model for the rooms at the building (Amity)
 """
 class Room(object):
-    """Room represents a Room in Amity"""
-    def __init__(self, name, members = []):
+    """Room represents a Room in the building"""
+    def __init__(self, name): #It has to have a name
         self.name = name
-        self.members = members
 
 class Office(Room):
-    """Office represents an Office"""
-    def __init__(self):
-        self.members = [6]
+    """Office is a Room (inherits from Room)"""
+    def __init__(self, name):
+        super(Office, self).__init__(name) #Room initialize with its Room.__init__
 
 class LivingSpace(Room):
-    """LivingSpace represents a LivingSpace"""
-    def __init__(self):
-        self.members = [4]
+    """LivingSpace is a Room"""
+    def __init__(self, name):
+        super(LivingSpace, self).__init__(name) ##Room initialize with its Room.__init__
 
 
 """
@@ -23,37 +22,17 @@ class LivingSpace(Room):
 """
 class Person(object):
     """Person represents an employee (Fellow and Staff)"""
-    def __init__(self, name, OfficeAllocated = False):
+    def __init__(self, name): #Every person has a name
         self.name = name
-        self.OfficeAllocated = OfficeAllocated
+        self.is_allocated_office = False
 
 class Staff(Person):
-    def check_allocation_status(self):
-        #checks status of the Staff in terms of Office allocation
-        print "Name : " + self.name + "\t"
-        print "Office : "
-        if self.OfficeAllocated == False:
-            print "NOT ALLOCATED \t"
-        else:
-            print self.OfficeAllocated + "\t"
-    pass
+    """Staff is a Person"""
+    def __init__(self):
+        super(Staff, self).__init__()
 
 class Fellow(Person):
-    def __init__(self, LS_Allocated = False, Interested_in_LS = "N"):
-        self.LS_Allocated = LS_Allocated
-        self.Interested_in_LS = Interested_in_LS
-
-    def check_allocation_status(self):
-        #checks status of the Fellow in terms of Office and Living Space allocation
-        print "Name : " + self.name + "\t"
-        print "Office : "
-        if self.OfficeAllocated == False:
-            print "NOT ALLOCATED \t"
-        else:
-            print self.OfficeAllocated + "\t"
-        print "Living Space : "
-        if self.LS_Allocated == False:
-            print "NOT ALLOCATED \t"
-        else:
-            print self.LS_Allocated + "\t"
-    pass
+    """Fellow is a Person"""
+    def __init__(self, is_interested): #Either Y or N (to be read from file)
+        super(Fellow, self).__init__()
+        self.is_interested = is_interested
